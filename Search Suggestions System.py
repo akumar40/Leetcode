16850a30c85756e69ca832faae156bbe6d697925
Avinash Kumar <akumar40@stevens.edu>
2022-08-07 00:57:36 -0400

    Search Suggestions System

---
+++ Search Suggestions System.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 00:39:20 2022
+
+@author: Avinash Kumar
+"""
+
+def suggestedProducts(products, searchWord):
+        res = []
+        n = -1
+        for idx in range(len(searchWord)):
+            if n == 0:
+                res.append([])
+                continue
+            char = searchWord[:idx+1]
+            match = list(filter(lambda x: x[:idx+1] == char, products))
+            n = len(match)
+            res.append(sorted(match)[:3])
+        return res
+    
+products = ["mobile","mouse","moneypot","monitor","mousepad"]
+searchWord = "mouse"
+res = suggestedProducts(products, searchWord)
+print(res)
